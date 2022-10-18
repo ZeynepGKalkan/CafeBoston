d1f340b3c501b6e0db68ae1adc36f66b5a56891d
ZeynepGül <zgkalkan@gmail.com>
2022-10-18 14:04:18 +0300

    README cahnages, classes are added

---
+++ CafeBoston.DATA/OrderState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA
+{
+    internal class OrderState
+    {
+    }
+}
