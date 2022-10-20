cc351483a5dd2586523de7f1b65f845f4b23586d
ZeynepGül <zgkalkan@gmail.com>
2022-10-20 17:19:11 +0300

    seriliazation,(data persistance), Table Move

--- CafeBoston.UI/OrderForm.cs
+++ CafeBoston.UI/OrderForm.cs
@@ -11,8 +11,10 @@ using System.Windows.Forms;
 
 namespace CafeBoston.UI
 {
+    public delegate void TableMoveHandler(int oldTableNo,int newTableN0);
     public partial class OrderForm : Form
     {
+        public event TableMoveHandler TableMoving;
         private readonly CafeData _db;
         private readonly Order _order;
         private readonly BindingList<OrderDetail> _orderDetails;
@@ -40,7 +42,23 @@ namespace CafeBoston.UI
             Text = $"Order (Table {_order.TableNo}-{_order.StarTime?.ToLongTimeString()})";
             lblTableNo.Text = _order.TableNo.ToString("00");
             lblTotalPrice.Text = _order.TotalPriceTRY;
+            LoadEmptyTableNo();
         }
+
+        private void LoadEmptyTableNo()
+        {
+            cboTableNo.DataSource = Enumerable.Range(1,_db.TableCount).
+                Where(x=>!_db.ActiveOrders.Any(y=>y.TableNo==x)).ToList();
+            //cboTableNo.Items.Clear();
+            //for (int i = 1; i < _db.TableCount; i++)
+            //{
+            //    if (_db.ActiveOrders.Any(x => x.TableNo != i))
+            //        cboTableNo.Items.Add(i);
+            
+            // }
+             
+            }
+        
 
         private void LoadProducts()
         {
@@ -98,6 +116,19 @@ namespace CafeBoston.UI
         private void btnCancel_Click(object sender, EventArgs e)
         {
             CompleteOrder("Are you sure that you want to cancel the order?", 0,OrderState.Canceled);
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (cboTableNo.SelectedIndex == -1) return;
+            int target = (int)cboTableNo.SelectedItem;
+            int oldTableNo=_order.TableNo;
+            _order.TableNo= target;
+            if (TableMoving != null)
+                TableMoving(oldTableNo, target);
+            UpdateTableInfo();
+            
+
         }
     }
 }
